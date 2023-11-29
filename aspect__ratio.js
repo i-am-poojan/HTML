36dd76e3672ffd3c73fd11851eb65162ab8e1dda
Poojan <p.poojan1762002@gmail.com>
2023-11-29 10:47:50 +0530

    added aspect_ratio.js

--- aspect__ratio.js
+++ aspect__ratio.js
@@ -1,1 +1,10 @@
 // how to calculate aspect ratio
+const width=500;
+// aspect ratio 16/9
+// here 16=width 9=height
+const r1=width/16
+console.log(r1*9);
+
+const height=250;
+const r2=height/9;
+console.log(r2*16);
